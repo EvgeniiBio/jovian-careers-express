875aa1a5aae8f9a0e81d0bc22aa9117fde639ae5
EvgeniiBio <69004324+EvgeniiBio@users.noreply.github.com>
2023-11-20 04:17:45 +0000

    Add mustache

--- src/app.js
+++ src/app.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const JOBS = require('./jobs');
+const mustacheExpress = require('mustache-express');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'pages/index.html')));
+// Configure mustache
+app.set('views', path.join(__dirname, 'pages'));
+app.set('view engine', 'mustache');
+app.engine('mustache', mustacheExpress());
+
+
+
+app.get('/', (req, res) => {
+    // res.sendFile(path.join(__dirname, 'pages/index.html'))
+    res.render('index', { jobs: JOBS}); // Render the template
+});
 
 const port = process.env.PORT || 3000; 
 
